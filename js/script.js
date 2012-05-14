77f4ebfdd08a8c7ed5406e410f8e811d3f06a164
ialcazar <israelalcazar@gmail.com>
2012-05-15 01:42:15 +0200

    Added Twitts

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 /* Author:
-
+	Israel Alcázar
 */
+
+jQuery(function($){
+        $(".tweet").tweet({
+        	avatar_size: 22,
+            count: 10,
+          	query: "from:spainjs",
+          	loading_text: "searching twitter..."
+        });
+    });
 
 
 
