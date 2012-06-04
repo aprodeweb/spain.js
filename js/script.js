f1550116da2709f23fce7437da7c739740d7f216
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-06-04 17:41:09 +0200

    Fixed indentation (tabs are now 2 spaces)

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 /* Author:
-	Israel Alcázar
-*/
+ Israel Alcázar
+ */
 
-jQuery(function($){
-    $(".tweet").tweet({
-        count: 3,
-      	query: "#spainjs OR from:@spainjs",
-      	loading_text: "searching twitter...",
-		template: '<span class="name">@{screen_name}</span><span class="tweet">{text}</span><span class="time">{time}</span>'
-    });
+jQuery(function ($) {
+  $(".tweet").tweet({
+    count:3,
+    query:"#spainjs OR from:@spainjs",
+    loading_text:"searching twitter...",
+    template:'<span class="name">@{screen_name}</span><span class="tweet">{text}</span><span class="time">{time}</span>'
+  });
 
-    $('.js-more','.jobs').bind('click',function(e){
-    	e.preventDefault();
-    	$(e.target).parent().toggleClass('less');
-    })
+  $('.js-more', '.jobs').bind('click', function (e) {
+    e.preventDefault();
+    $(e.target).parent().toggleClass('less');
+  })
 });
 
 
